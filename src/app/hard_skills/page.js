13677211ecff9cb719ff080f62acbcfb2f812628
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-12-04 21:47:52 +0100

    Intégration card langages et outils

--- src/app/hard_skills/page.js
+++ src/app/hard_skills/page.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
+import Skills from '@/components/hardSkills'
+
 const HardSkills = () => {
     return (
-        <div>
+        <main>
           <Header />
           <Navbar />
-          page</div>
+          <Skills />
+          </main>
       )
 }
 
